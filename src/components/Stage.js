51296f37f46f7b105f876438263e967f8fac42a2
Ignacio Bejarano <ignbejarano@gmail.com>
2019-09-01 12:35:49 -0300

    Init board Array

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -3,7 +3,10 @@ import React from 'react';
 import Cell from './Cell';
 
 const Stage = ({ stage }) => (
-    <div><Cell /></div>
+    <div>
+        {/* stage is Array(rows)) of Arrays(cols) with [0 ,'clear'] in each individual cell  */}
+        {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} /> ))}
+    </div>
 )
 
 export default Stage;
