b25814e13c1b416743e4860cc1e9abe248031a1d
Ignacio Bejarano <ignbejarano@gmail.com>
2019-09-03 19:11:02 -0300

    Set sweep rows function

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -3,8 +3,22 @@ import { createStage } from '../gameHelpers';
 
 export const useStage = (player, resetPlayer) => {
     const [stage, setStage] = useState(createStage());
+    const [ rowsCleared, setRowsCleared ] = useState(0);
 
     useEffect(() => {
+        setRowsCleared(0);
+
+        const sweepRows = newStage =>
+            newStage.reduce((accum, row) => {
+                if(row.findIndex(cell => cell[0] ===0) === -1){
+                    setRowsCleared(prev => prev + 1);
+                    accum.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+                    return accum
+                }
+                accum.push(row)
+                return accum;
+            }, [])
+
         const updateStage = prevStage => {
             const newStage = prevStage.map(row => 
                 row.map( cell => (cell[1] ==='clear'? [0,'clear'] : cell)),
@@ -25,6 +39,7 @@ export const useStage = (player, resetPlayer) => {
         // check if we collided
         if (player.collided){
             resetPlayer()
+            return sweepRows(newStage);
         }
 
         return newStage;
@@ -34,5 +49,5 @@ export const useStage = (player, resetPlayer) => {
 
     }, [player, resetPlayer]);
 
-    return [stage, setStage];
+    return [stage, setStage, rowsCleared];
 }
